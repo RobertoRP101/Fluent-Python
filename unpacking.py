63aede49ef88756124d831c87fd15bcf04ddedec
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-10-07 23:54:24 -0600

    parallel assigment

---
+++ unpacking.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Parallel assigment
+def parallel_assigment():
+    '''
+    This method needs a specific
+    number of variables to work
+    properly, based on of the
+    number of items inside the
+    iterator
+    '''
+    coordinates = (33.9425, -118.408056)
+    latitude, longitude = coordinates
+    print(latitude, longitude)
