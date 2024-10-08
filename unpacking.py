c4c20f0f64e27598f324b8554d968421803afb1f
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-10-07 23:56:50 -0600

    grab excess items

--- unpacking.py
+++ unpacking.py
@@ -13,6 +13,7 @@ def parallel_assigment():
     latitude, longitude = coordinates
     print(latitude, longitude)
     
+    
 # Swapping values
 def swapping_values():
     numbers = [x for x in range(0,50,10)]
@@ -21,6 +22,7 @@ def swapping_values():
     numbers[0], numbers[-1] = numbers[-1], numbers[0]
     print('After swapping')
     print(numbers)
+    
     
 # Prefixing an argument
 def prefixing_an_argument():
@@ -34,3 +36,22 @@ def prefixing_an_argument():
     print(divmod(*t))
     quotient, remainder = divmod(*t)
     print(f'quotient {quotient}, remaider{remainder}')
+    
+    
+# Grab arbitrary excess arguments
+def grab_excess_items():
+    '''
+    This method lets us show how
+    to take a group of items
+    without defining additional
+    variables
+    '''
+    print('Range 5')
+    a, b, *excess = range(2)
+    print(f'a: {a}, b: {b}, excess: {excess}')
+    print('Range 10')
+    a, b, *excess = range(10)
+    print(f'a: {a}, b: {b}, excess: {excess}')
+    print('Random range')
+    a, b, *excess = np.random.normal(0,1,100)
+    print(f'a: {a}, b: {b}, excess: {excess}')
