8f038a7eede8fbca620e2c99cc611257698a996f
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-10-10 23:57:41 -0600

    sorting list with function

--- lists.py
+++ lists.py
@@ -13,3 +13,16 @@ def sort_list():
     print(f'Id normal list: {id(l)}')
     sorted_l = sorted(l, reverse=True)
     print(f'This function returns a copy: {sorted_l}\nand the reference is: {id(sorted_l)}')
+    
+    
+# Sorting the original list 
+def sort_list_inplace():
+    '''
+    This method uses the sorted method,
+    this means that we are not creating
+    a new list, is in place
+    '''
+    l = [ f'{x}' for x in range(0,-20,-1)]
+    print(f'Id normal list: {id(l)}')
+    l.sort(key=len, reverse=True)
+    print(f'This function don\'t return a copy: {l}\nand the reference is: {id(l)}')
