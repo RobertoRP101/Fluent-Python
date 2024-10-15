b6aebed7c11b487b2f67a04e9aed7c015763f617
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-10-14 23:55:09 -0600

    parrallel_assigment review

---
+++ review.py
@@ -0,0 +1,6 @@
+def parallel_assignment():
+    coordinates = (33.9425, -118.408056)
+    latitude, longitude = coordinates  # Directly assigns values
+    print("Latitude:", latitude, "Longitude:", longitude)
+
+
