a2cc893c949020dc07bbf9eddb38bfeb4e907723
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-10-17 23:20:07 -0600

    pattern matching using mapping

--- Chapter three/dictionaries.py
+++ Chapter three/dictionaries.py
@@ -45,3 +45,15 @@ def merge_mapp_update():
 print(merge_mapp())
 print(merge_mapp_update())
     
+def get_creators(record: dict) -> list:
+ match record:
+    case {'type': 'book', 'api': 2, 'authors': [*names]}: 
+        return names
+    case {'type': 'book', 'api': 1, 'author': name}: 
+        return [name]
+    case {'type': 'book'}: 
+        raise ValueError(f"Invalid 'book' record: {record!r}")
+    case {'type': 'movie', 'director': name}: 
+        return [name]
+    case _: 
+        raise ValueError(f'Invalid record: {record!r}')
