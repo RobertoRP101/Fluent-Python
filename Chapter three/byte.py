110e90799e6f793a1f7380f1ee386073a30ee6cf
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-10-23 09:07:20 -0600

    decode with different codecs

--- Chapter three/byte.py
+++ Chapter three/byte.py
@@ -29,3 +29,16 @@ def encode_by_codecs():
 
 # byte_and_arraybyte()
 encode_by_codecs()
+
+
+def error_decode_encode():
+    octets = b'Montr\xe9al' 
+    print(octets.decode('cp1252') )
+    print(octets.decode('iso8859_7') )
+    print(octets.decode('koi8_r') )
+    print(octets.decode('utf_8') )
+    print(octets.decode('utf_8', errors='replace') )
+    
+error_decode_encode()
+
+    
