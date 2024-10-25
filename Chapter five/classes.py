ea6b89f125b0c79a401784ffe851c894a369f39c
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-10-25 09:41:02 -0600

    using the typing to namedtuple

--- Chapter five/classes.py
+++ Chapter five/classes.py
@@ -1,3 +1,6 @@
+import typing
+from collections import namedtuple
+
 class Coordinates():
     def __init__(self, lat, lon) -> None:
         self.lat = lat
@@ -11,4 +14,26 @@ def basic_class_case():
     print(f'Are the classes equals: {location == moscow}')
     print(f'Are their attributes of each class equals: {(moscow.lat, moscow.lon) == (location.lat, location.lon)}')
 
-basic_class_case()
+# basic_class_case()
+
+
+# Since this point we can create subclasses to
+# save data
+
+def collection_use():
+    Coordinate = namedtuple('Coordinate', 'lat lon')
+    print(f'Is subclass of tuple: {issubclass(Coordinate, tuple)}')
+    moscow = Coordinate(55.756, 37.617)
+    print(moscow)
+    print(f'Is equal the instance moscow with a ne object with the same coordinates: {moscow == Coordinate(lat=55.756, lon=37.617)}')
+    
+# collection_use()
+
+def using_typing_coordinate():
+    # Coordinate = namedtuple('Coordinate', 'lat lon')
+    Coordinate = typing.NamedTuple('Coordinate', [('lat', float), ('lon', float)])
+    print(f'Is Coordinate a subclass of tuple: {issubclass(Coordinate, tuple)}')
+    print(typing.get_type_hints(Coordinate))
+    
+    
+using_typing_coordinate()
