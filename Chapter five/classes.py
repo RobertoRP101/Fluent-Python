d310630772319bbaa570eba47b63e402d5009b25
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-10-30 23:56:48 -0600

    typing namedtuple

--- Chapter five/classes.py
+++ Chapter five/classes.py
@@ -37,3 +37,13 @@ def using_typing_coordinate():
     
     
 using_typing_coordinate()
+
+
+from typing import NamedTuple
+class Coordinate(NamedTuple):
+ lat: float
+ lon: float
+ def __str__(self):
+    ns = 'N' if self.lat >= 0 else 'S'
+    we = 'E' if self.lon >= 0 else 'W'
+    return f'{abs(self.lat):.1f}°{ns}, {abs(self.lon):.1f}°{we}'
